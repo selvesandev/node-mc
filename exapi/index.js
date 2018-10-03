6e18d535024cd42a3e5541dc52c47adfac59bb96
selvesan <selvesan@selvesans-MacBook-Pro.local>
2018-10-03 13:25:15 +0545

    express basics

--- exapi/index.js
+++ exapi/index.js
@@ -1,1 +1,26 @@
-require('express');
+const express = require('express');
+const Joi = require('joi');
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        res.status(400).send(result.error);
+    }
+    res.send(req.body);
+});
+
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
